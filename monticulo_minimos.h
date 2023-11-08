0929a1defe8f998dc09d28049b4559f951bbd251
alex-mosquera <alex.mosquera@udc.es>
2023-11-08 14:03:21 +0100

    cambio realizado

--- monticulo_minimos.h
+++ monticulo_minimos.h
@@ -5,4 +5,12 @@
 #ifndef ALGORITMOS_P3_MONTICULO_MINIMOS_H
 #define ALGORITMOS_P3_MONTICULO_MINIMOS_H
 
+typedef struct monticulo {
+    int ultimo; 
+    int *vector; 
+}tMount; 
+
+void crearMonticulo (int v[], int n, tMount M); 
+int quitarMenor (tMount M)
+
 #endif //ALGORITMOS_P3_MONTICULO_MINIMOS_H
