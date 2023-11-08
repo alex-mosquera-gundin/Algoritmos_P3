3f9c21e2c1950961b0b97886aaca6384de5b26ff
Heros <heros.vicente@gmail.com>
2023-11-08 13:55:39 +0100

    hecho crear arrays

--- ordenacion_monticulos.c
+++ ordenacion_monticulos.c
@@ -1,5 +1,45 @@
-//
-// Created by Heros on 08/11/2023.
-//
-
 #include "ordenacion_monticulos.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+//rellena el array de forma aleatoria
+void rellenar_array_aleatorio(int array[], int len){
+    int i;
+
+    //inicializar semilla
+    srand(time(NULL));
+
+    //rellenar con numeros aleatorios enre len y - len
+    for(i = 0; i < len; i++)
+        array[i] =(rand() % (2 * len)) - len;
+}
+
+//rellena el array de forma ascendente
+void rellenar_array_ascendente(int array[], int len){
+    int i;
+    for(i = 0; i < len; i++)
+        array[i] = i + 1;
+}
+
+//rellena el array de forma descendente
+void rellenar_array_descendente(int array[], int len){
+    int i;
+    for(i = 0; i < len; i++)
+        array[i] = len - i;
+}
+
+//ordena el array por medio de monticulos
+void ordenar_array_por_monticulos(int array[], int len){
+
+}
+
+//devuelve un 1 si el array está ordenado y un 0 si no
+int es_array_ordenado(int array[], int len){
+    int cond = 1, i;
+    for(i = 1; i < len && cond; i++)
+        if(array[i-1] > array[i])
+            cond = 0;
+
+    return cond;
+}
--- ordenacion_monticulos.h
+++ ordenacion_monticulos.h
@@ -5,4 +5,11 @@
 #ifndef ALGORITMOS_P3_ORDENACION_MONTICULOS_H
 #define ALGORITMOS_P3_ORDENACION_MONTICULOS_H
 
+void ordenar_array_por_monticulos(int array[], int len);
+void rellenar_array_aleatorio(int array[], int len);
+void rellenar_array_ascendente(int array[], int len);
+void rellenar_array_descendente(int array[], int len);
+int es_array_ordenado(int array[], int len);
+
+
 #endif //ALGORITMOS_P3_ORDENACION_MONTICULOS_H
